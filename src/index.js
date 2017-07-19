3b3305575a6f5711dd06bb82626afc6ba3174692
nik frank <nikbfrank@gmail.com>
2017-07-19 17:06:03 +0300

    install tahini

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { bootApp } from 'tahini';
+
+const TahiniApp = bootApp().getDevice(App);
+
+ReactDOM.render(
+  <TahiniApp />,
+  document.getElementById('root'));
 registerServiceWorker();
