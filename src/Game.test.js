3ed9033fb7320483c53aadf947125ec0d4d66e88
nik frank <nikbfrank@gmail.com>
2017-07-19 22:04:11 +0300

    simulate button click spy

--- src/Game.test.js
+++ src/Game.test.js
@@ -20,7 +20,34 @@ it('renders the current guess', () => {
 
 
 it('provides up and down buttons for each dot', () => {
-  
+  const state = fromJS({
+    guess: [ 2, 3, 4, 5 ]
+  });
+
+  const incGuessDot = jest.fn();
+  const decGuessDot = jest.fn();
+
+  const p = mount(<Game subState={state}
+                        incGuessDot={incGuessDot}
+                        decGuessDot={decGuessDot} />);
+
+  const cols = p.find('.guess-col');
+
+  cols.forEach( (col, i)=> {
+    // click buttons, each expect call with index
+    const buttons = col.find('button');
+
+    const prevIncs = incGuessDot.mock.calls.length;
+    buttons.at(0).simulate('click');
+    expect( incGuessDot.mock.calls.length ).toEqual( prevIncs + 1 );
+
+    const prevDecs = decGuessDot.mock.calls.length;
+    buttons.at(1).simulate('click');
+    expect( decGuessDot.mock.calls.length ).toEqual( prevDecs + 1 );
+  });
+
+  expect( incGuessDot.mock.calls.length ).toEqual( cols.length );
+  expect( decGuessDot.mock.calls.length ).toEqual( cols.length );
 });
 
 
