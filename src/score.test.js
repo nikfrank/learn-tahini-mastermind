aad7a08aff2f4b4d25d364ac48ffc8017a5fd300
nik frank <nikbfrank@gmail.com>
2017-07-21 02:38:22 +0300

    43 failing cases

--- src/score.test.js
+++ src/score.test.js
@@ -9,6 +9,59 @@ const cases = [
   { code: [ 0, 0, 0, 0 ], guess: [ 1, 1, 1, 1 ], result: [ 0, 0 ] },
 
   
+  { code: [ 0, 1, 2, 3 ], guess: [ 4, 0, 0, 0 ], result: [ 0, 1 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 4, 0, 0, 0 ], result: [ 0, 1 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 1, 4, 4, 4 ], result: [ 0, 1 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 4, 0, 2, 2 ], result: [ 0, 1 ] },
+  { code: [ 0, 1, 1, 1 ], guess: [ 4, 0, 0, 0 ], result: [ 0, 1 ] },
+  { code: [ 0, 1, 1, 1 ], guess: [ 1, 4, 4, 4 ], result: [ 0, 1 ] },
+  
+  { code: [ 0, 1, 2, 3 ], guess: [ 1, 0, 0, 0 ], result: [ 0, 2 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 1, 0, 0, 0 ], result: [ 0, 2 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 1, 4, 4, 1 ], result: [ 0, 2 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 4, 0, 2, 4 ], result: [ 0, 2 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 1, 0, 2, 2 ], result: [ 0, 2 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 2, 0, 2, 0 ], result: [ 0, 2 ] },
+  { code: [ 0, 1, 1, 1 ], guess: [ 1, 0, 0, 0 ], result: [ 0, 2 ] },
+  
+  { code: [ 0, 1, 2, 3 ], guess: [ 1, 0, 3, 0 ], result: [ 0, 3 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 1, 0, 3, 1 ], result: [ 0, 3 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 1, 0, 2, 4 ], result: [ 0, 3 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 1, 0, 1, 4 ], result: [ 0, 3 ] },
+
+  { code: [ 0, 1, 2, 3 ], guess: [ 1, 0, 3, 2 ], result: [ 0, 4 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 1, 0, 2, 1 ], result: [ 0, 4 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 1, 0, 1, 0 ], result: [ 0, 4 ] },
+
+
+  { code: [ 0, 1, 2, 3 ], guess: [ 0, 0, 1, 0 ], result: [ 1, 1 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 0, 0, 2, 0 ], result: [ 1, 1 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 4, 1, 0, 4 ], result: [ 1, 1 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 1, 1, 4, 4 ], result: [ 1, 1 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 0, 0, 4, 4 ], result: [ 1, 1 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 0, 4, 1, 4 ], result: [ 1, 1 ] },
+  { code: [ 0, 1, 1, 1 ], guess: [ 1, 0, 4, 4 ], result: [ 1, 1 ] },
+
+  { code: [ 0, 1, 2, 3 ], guess: [ 0, 2, 1, 4 ], result: [ 1, 2 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 0, 2, 4, 1 ], result: [ 1, 2 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 2, 0, 1, 0 ], result: [ 1, 2 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 0, 0, 1, 2 ], result: [ 1, 2 ] },
+  { code: [ 0, 1, 1, 1 ], guess: [ 1, 0, 1, 2 ], result: [ 1, 2 ] },
+  
+  { code: [ 0, 1, 2, 3 ], guess: [ 0, 2, 3, 1 ], result: [ 1, 3 ] },
+  
+  { code: [ 0, 1, 2, 3 ], guess: [ 0, 1, 3, 1 ], result: [ 2, 1 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 0, 1, 3, 1 ], result: [ 2, 1 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 3, 1, 1, 0 ], result: [ 2, 1 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 0, 1, 1, 2 ], result: [ 2, 1 ] },
+  { code: [ 0, 1, 1, 1 ], guess: [ 1, 1, 1, 2 ], result: [ 2, 1 ] },
+  
+  { code: [ 0, 1, 2, 3 ], guess: [ 0, 1, 3, 2 ], result: [ 2, 2 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 0, 1, 2, 1 ], result: [ 2, 2 ] },
+  { code: [ 0, 1, 1, 2 ], guess: [ 2, 1, 1, 0 ], result: [ 2, 2 ] },
+  { code: [ 0, 1, 0, 1 ], guess: [ 0, 1, 1, 0 ], result: [ 2, 2 ] },
+  { code: [ 0, 1, 1, 1 ], guess: [ 1, 0, 1, 1 ], result: [ 2, 2 ] },
+  
 ];
 
 
