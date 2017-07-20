189aa6fd3c19a7bd613133077680a0d63c68da77
nik frank <nikbfrank@gmail.com>
2017-07-21 01:58:38 +0300

    failing test for exact matches

--- src/score.test.js
+++ src/score.test.js
@@ -1,6 +1,18 @@
-const cases = [];
+import score from './score';
 
+const cases = [
+  { code: [ 0, 0, 0, 0 ], guess: [ 0, 0, 0, 0 ], result: [ 4, 0 ] },
+  { code: [ 0, 0, 0, 0 ], guess: [ 0, 0, 0, 1 ], result: [ 3, 0 ] },
+  { code: [ 0, 0, 0, 0 ], guess: [ 0, 0, 1, 1 ], result: [ 2, 0 ] },
+  { code: [ 0, 0, 0, 0 ], guess: [ 0, 1, 1, 1 ], result: [ 1, 0 ] },
+  { code: [ 0, 0, 0, 0 ], guess: [ 1, 1, 1, 1 ], result: [ 0, 0 ] },
 
-it('calculates the score correctly for each case', () => {
   
+];
+
+
+cases.forEach( ({ code, guess, result })=> {
+  it('calculates the score correctly for each case', () => {
+    expect( score(code, guess) ).toEqual( result );
+  });
 });
